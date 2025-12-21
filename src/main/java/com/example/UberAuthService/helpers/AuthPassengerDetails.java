984a82fcd91ee229844d0286fcf33f8f339b3013
Abhishek Kumar Singh <hrithiksinghrocks31@gmail.com>
2025-12-21 13:24:18 +0530

    Added UserDetails class and UserDetailsServiceImpl given by spring security

---
+++ src/main/java/com/example/UberAuthService/helpers/AuthPassengerDetails.java
@@ -0,0 +1,31 @@
+package com.example.UberAuthService.helpers;
+
+import com.example.UberAuthService.models.Passenger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+//why we need this class?
+//Because spring security works on UserDetails polymorphic type for auth
+
+public class AuthPassengerDetails extends Passenger implements UserDetails {
+    private final String username; //the unique identifier for the user is email but spring security convention refers it as a username
+
+    private final String password;
+
+    public AuthPassengerDetails(Passenger passenger){
+        this.username=passenger.getEmail();
+        this.password=passenger.getPassword();
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ src/main/java/com/example/UberAuthService/services/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.UberAuthService.services;
+
+import com.example.UberAuthService.helpers.AuthPassengerDetails;
+import com.example.UberAuthService.models.Passenger;
+import com.example.UberAuthService.repositories.PassengerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+/*
+* This class is responsible for loading the user in the form of UserDetails object for auth.
+*/
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final PassengerRepository passengerRepository;
+
+    public UserDetailsServiceImpl(PassengerRepository passengerRepository){
+        this.passengerRepository=passengerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger=passengerRepository.findPassengerByEmail(email);
+        if(passenger.isPresent()){
+            return new AuthPassengerDetails(passenger.get());
+        }else {
+            throw new UsernameNotFoundException("cannot find the Passenger by the given Email");
+        }
+    }
+}
